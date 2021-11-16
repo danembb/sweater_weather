d8e0edc52d992370645c84034f5dbf53da130bc7
danembb <dbrophy720@gmail.com>
2021-11-15 23:41:17 -0700

    finish 1b

--- app/facades/unsplash_facade.rb
+++ app/facades/unsplash_facade.rb
@@ -3,6 +3,7 @@ class UnsplashFacade
     def get_background_image(search_param)
       response = UnsplashService.get_image(search_param)
       results = response[:results].first
+
       BackgroundImage.new(results)
     end
   end
