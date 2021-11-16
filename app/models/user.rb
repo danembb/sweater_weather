2b38d38f27ac4fdd19f6f506eccf2f667ee04299
danembb <dbrophy720@gmail.com>
2021-11-16 01:53:22 -0700

    Create validations for user

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :email, uniqueness: true, presence: true
+  validates :password_digest, presence: true
+  validates :api_key, uniqueness: true, presence: true
+
+  has_secure_password
+end
