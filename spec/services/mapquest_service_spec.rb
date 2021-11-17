7f881adde4f2d8d7b971163ab821e04801dddca5
danembb <dbrophy720@gmail.com>
2021-11-16 22:12:47 -0700

    Create get_route method with testing in mapquest_service

--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -12,4 +12,17 @@ RSpec.describe 'mapquest service' do
     expect(coordinates[:lat]).to eq(42.31952)
     expect(coordinates[:lng]).to eq(-72.629756)
   end
+
+  it 'can return route information given start and end locations', :vcr do
+    response = MapquestService.get_route('northampton,ma', 'lakewood,co')
+
+    expect(response).to be_a(Hash)
+    expect(response).to have_key(:route)
+    expect(response[:route]).to be_a(Hash)
+    expect(response[:route]).to have_key(:formattedTime)
+    expect(response[:route]).to have_key(:locations)
+    expect(response[:route][:locations]).to be_an(Array)
+    expect(response[:route][:locations][0]).to have_key(:adminArea5)
+    expect(response[:route][:locations][1]).to have_key(:adminArea5)
+  end
 end
